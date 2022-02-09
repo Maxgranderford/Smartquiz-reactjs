19e67d80008b16835715aa8a811feac3df5f11d7
max <maxpushkov999@gmail.com>
2022-02-09 15:35:10 +0300

    first commit

---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,26 @@
+export const Questions = [
+    {
+        prompt: "What is 1+3?",
+        optionA: "18",
+        optionB: "298",
+        optionC: "13",
+        optionD: "4",
+        answer: "A",
+    },
+    {
+        prompt: "Which is a Streaming Platform?",
+        optionA: "Twitter",
+        optionB: "Twitch",
+        optionC: "Google",
+        optionD: "Robinhood",
+        answer: "B",
+    },
+    {
+        prompt: "Best person in the world?",
+        optionA: "Pablo",
+        optionB: "Elon Musc",
+        optionC: "MAX!",
+        optionD: "nelli",
+        answer: "C",
+    }
+]
